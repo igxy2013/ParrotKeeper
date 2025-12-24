c234d62a81b38e022100981433254d3a6916277a
igxy2013 <gxymp3@qq.com>
2025-12-25 06:30:05 +0800

    chore: 更新vite配置中的allowedHosts列表
    添加aibim.xyz和acbim.cn到允许的主机列表，以支持新的域名访问

--- web/vite.config.js
+++ web/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
-    allowedHosts: ['acbim.fun'],
+    allowedHosts: ['acbim.fun', 'aibim.xyz','acbim.cn'],
     proxy: {
       '/api': {
         target: 'http://localhost:5075',
